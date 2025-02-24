b8dea2d83f75451040c658feb99324679014af8d
KOT-TIPO <t143alena@gmail.com>
2025-02-24 13:21:05 +0300

    Add files via upload

---
+++ interface/IProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagnov
+{
+    public class IProduct
+    {
+        string Name { get; set; }
+        decimal Price { get; set; }
+    }
+}
+public class Product
+{ 
+
+}
