160c0ecdb55215ca6332295756953fa5198c4b51
KOT-TIPO <t143alena@gmail.com>
2025-02-24 13:23:41 +0300

    Add files via upload

---
+++ reg.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Yagnov
+{
+    /// <summary>
+    /// Логика взаимодействия для reg.xaml
+    /// </summary>
+    public partial class reg : Window
+    {
+        private const string CorrectLogin = "KKT";
+        private const string CorrectPass = "best";
+        
+        public reg()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tbxLogin.Text;
+            string pass = tbxPass.Text;
+            if (login == CorrectLogin && pass == CorrectPass)
+            { }
+
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
